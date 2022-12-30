0a3a941096475dee6da4d6a40abe511b9e0b502c
viajohnt <118217497+viajohnt@users.noreply.github.com>
2022-12-30 16:10:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const updatedEmployee = { ...employee };
+  updatedEmployee[key] = value;
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+const employee = { name: 'Sam' };
+const key = 'streetAddress';
+const value = '11 Broadway';
+
+console.log(updateEmployeeWithKeyAndValue(employee, key, value));
+
+console.log(employee);
